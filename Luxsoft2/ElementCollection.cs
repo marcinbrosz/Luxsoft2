25c35fefafe65eababbc1cbfa77f3529a3c289df
marcinbrosz <marcio_br@interia.pl>
2018-05-09 14:46:51 +0200

    Add project files.

---
+++ Luxsoft2/ElementCollection.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luxsoft2
+{
+    class ElementCollection
+    {
+        public class Element
+        {
+            private int num;
+            private String name;
+            private int age;
+
+            public int Num
+            {
+                get { return num; }
+                set { num = value; }
+            }
+            public String Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public int Age
+            {
+                get { return age; }
+                set { age = value; }
+            }
+        }
+
+        public static Collection<Element> uniqueColection(Collection<Element> input)
+        {
+
+            return new Collection<Element>(input
+                .Where(y => y.Age > 20)//age>20
+                .GroupBy(x => x.Num)//all elements are unique by "num"
+                .Select(x => x.ElementAt(0)).ToList());//any of input elements
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Collection<Element> element =
+                new Collection<Element>{ new Element {Num=3 ,Name="Barley", Age=23 },
+                                new Element { Num=3,Name="Boots", Age=45 },
+                                new Element { Num=1,Name="Whiskers", Age=1 },
+                                new Element { Num=2,Name="Daisy", Age=56 } };
+
+            Collection<Element> element2 = uniqueColection(element);
+
+
+        }
+    }
+}
---
+++ Luxsoft2/TreesNodes.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luxsoft2
+{
+
+    class TreesNodes
+    {
+        class BTN
+        {
+            private int val;
+            private BTN left;
+            private BTN right;
+
+
+            public int Val
+            {
+                get { return val; }
+                set { val = value; }
+            }
+            public BTN Left
+            {
+                get { return left; }
+                set { left = value; }
+            }
+            public BTN Right
+            {
+                get { return right; }
+                set { right = value; }
+            }
+
+            public override bool Equals(object obj)
+            {
+                BTN _obj = (BTN)obj;
+                return  Val == _obj.Val 
+                        &&object.Equals(Left, _obj.Left) 
+                        && object.Equals(Right, _obj.Right);
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            BTN bTN2 = new BTN();
+            bTN2.Val = 4;
+            //bTN.Left = bTN2;
+            //bTN.Right = bTN3;
+            BTN bTN3 = new BTN();
+            bTN3.Val = 6;
+            object.Equals(bTN2, bTN3);
+            //bool asd = object.Equals(bTN2, bTN3);
+
+            BTN bTN = new BTN();
+            bTN.Val = 1;
+            bTN.Left = bTN2;
+            bTN.Right = bTN3;
+            //
+
+
+
+            BTN bTN22 = new BTN();
+            bTN22.Val = 4;
+            //bTN22.Left = bTN;
+
+            //bTN.Left = bTN2;
+            //bTN.Right = bTN3;
+            BTN bTN32 = new BTN();
+            bTN32.Val = 6;
+
+
+
+            BTN bTN12 = new BTN();
+            bTN12.Val = 1;
+            bTN12.Left = bTN22;
+            bTN12.Right = bTN32;
+            ////
+
+            bool a=bTN.Equals(bTN12);
+
+
+        }
+    }
+}
